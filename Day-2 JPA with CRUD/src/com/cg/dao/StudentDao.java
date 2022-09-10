c540ffa94436401684d16e7da877a4d9412c8742
Lenovo <Lenovo@LAPTOP-4FF204OR>
2022-09-10 09:11:34 +0530

    First Commit

---
+++ Day-2 JPA with CRUD/src/com/cg/dao/StudentDao.java
@@ -0,0 +1,16 @@
+package com.cg.dao;
+
+import com.cg.entities.Student;
+
+public interface StudentDao 
+{
+	//abstract method by default
+	public abstract Student getStudentById(int rollno);
+	public abstract void addStudent(Student s);
+	public abstract void deleteStudent(Student s);
+	public abstract void updateStudent(Student s);
+
+	public abstract void commitTransaction();
+	public abstract void beginTransaction();
+	
+}
