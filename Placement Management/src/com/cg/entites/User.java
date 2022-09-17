e88788be72d2975707e136833c4ca9562a5244bf
Lenovo <Lenovo@LAPTOP-4FF204OR>
2022-09-17 23:52:46 +0530

    First commit

---
+++ Placement Management/src/com/cg/entites/User.java
@@ -0,0 +1,82 @@
+package com.cg.entites;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="type")
+	private String type;
+	
+	@Column(name="password")
+	private String password;
+	 
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="college_id")
+	private College college;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="admin_id")
+	private Admin admin;
+
+	//getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public College getCollege() {
+		return college;
+	}
+
+	public void setCollege(College college) {
+		this.college = college;
+	}
+	
+	
+
+}
---
+++ Placement Management/src/com/cg/service/IPlacementService.java
@@ -0,0 +1,12 @@
+package com.cg.service;
+
+import com.cg.entites.Placement;
+
+public interface IPlacementService 
+{
+	public abstract Placement addPlacement(Placement placement);
+	public abstract Placement updatePlacement(Placement placement);
+	public abstract Placement searchPlacement(int id);
+
+
+}
